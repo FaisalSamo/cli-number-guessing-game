91d5ebcd720a52cb8a86ebc21156b1b93c5706aa
Faisal Samo <161488122+FaisalSamo@users.noreply.github.com>
2024-03-28 19:43:23 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import  inquirer from "inquirer";
+
+const RandomNumber=Math.floor(Math.random()*6+1); //Computer generate Random number's within the 6
+
+//user input for guessnumber
+const numbers= await inquirer.prompt([
+    {name:"userGuessnumber" , type:"number" , message:"please guess number between 1-6 ="}
+]);
+
+//compare user input and with computer generated number and show result
+if(numbers.userGuessnumber===numbers.RandomNumber) 
+{
+console.log("CONGRATS! Your guessed number is Correct ");
+}
+else
+{
+ console.log("your guessed number is not correct");
+}
